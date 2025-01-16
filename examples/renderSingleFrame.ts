f43ef530095fe7367c7b5f0d9a510cff4e542338
Brandon Keepers <brandon@opensoul.org>
2025-01-16 09:34:36 -0500

    Convert examples to ts

---
+++ examples/renderSingleFrame.ts
@@ -0,0 +1,13 @@
+import JSON from 'json5';
+import fsExtra from 'fs-extra';
+
+// eslint-disable-next-line import/named
+import { renderSingleFrame } from 'editly';
+
+(async () => {
+  await renderSingleFrame({
+    time: 0,
+    clips: JSON.parse(await fsExtra.readFile('./videos.json5', 'utf-8')).clips,
+    outPath: 'renderSingleFrame.png'
+  });
+})().catch(console.error);
