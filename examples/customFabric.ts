5042203b8a24e2a5b11e8bde40be8ed780dfecd8
Brandon Keepers <brandon@opensoul.org>
2025-01-19 16:05:03 -0500

    Show example of registering custom font

--- examples/customFabric.ts
+++ examples/customFabric.ts
@@ -1,17 +1,21 @@
 import editly from 'editly';
 import type { CustomFabricFunctionArgs, CustomFabricFunctionCallbacks } from 'editly';
+import { registerFont } from 'canvas';
+
+registerFont('./assets/Patua_One/PatuaOne-Regular.ttf', { family: 'Patua One' });
 
 function func({ width, height, fabric }: CustomFabricFunctionArgs): CustomFabricFunctionCallbacks {
   return {
     async onRender(progress, canvas) {
       canvas.backgroundColor = 'hsl(33, 100%, 50%)';
 
-      const text = new fabric.Text(`PROGRESS\n${Math.floor(progress * 100)}%`, {
+      const text = new fabric.FabricText(`PROGRESS\n${Math.floor(progress * 100)}%`, {
         originX: 'center',
         originY: 'center',
         left: width / 2,
         top: (height / 2) * (1 + (progress * 0.1 - 0.05)),
         fontSize: 20,
+        fontFamily: 'Patua One',
         textAlign: 'center',
         fill: 'white',
       });
