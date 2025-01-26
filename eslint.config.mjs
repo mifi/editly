1fa43002319d1d26204b10ea21b602803b6d1e83
Brandon Keepers <brandon@opensoul.org>
2025-01-26 12:37:20 -0500

    Configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  eslintConfigPrettier,
   { ignores: ["dist/"] },
 );
