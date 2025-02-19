4b63c073e270cb801dd7389dd06af56b555d4751
Brandon Keepers <brandon@opensoul.org>
2025-02-19 09:05:55 -0500

    Use AbortController to stop ffmpeg process

--- src/sources/video.ts
+++ src/sources/video.ts
@@ -1,4 +1,5 @@
 import assert from "assert";
+import { ExecaError } from "execa";
 import * as fabric from "fabric/node";
 import { defineFrameSource } from "../api/index.js";
 import { ffmpeg, readFileStreams } from "../ffmpeg.js";
@@ -129,12 +130,23 @@ export default defineFrameSource<VideoLayer>("video", async (options) => {
     "-",
   ];
 
+  const controller = new AbortController();
+
   const ps = ffmpeg(args, {
     encoding: "buffer",
     buffer: false,
     stdin: "ignore",
     stdout: "pipe",
     stderr: process.stderr,
+    // ffmpeg doesn't like to stop, force it
+    killSignal: "SIGKILL",
+    cancelSignal: controller.signal,
+  });
+
+  // Ignore errors if the process is aborted
+  ps.catch((err: ExecaError) => {
+    if (!err.isCanceled) throw err;
+    if (verbose) console.log("ffmpeg process aborted", path);
   });
 
   const stream = ps.stdout!;
@@ -273,7 +285,7 @@ export default defineFrameSource<VideoLayer>("video", async (options) => {
 
   const close = () => {
     if (verbose) console.log("Close", path);
-    ps.kill();
+    controller.abort();
   };
 
   return {
